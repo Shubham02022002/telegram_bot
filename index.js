bf303d9c617e6d63b05093661080d9c31351e449
Shubham <chaudhary2001shubham@gmail.com>
2025-04-13 21:44:10 +0530

    V2

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-const path = require("path");
-const fs = require("fs");
-const util = require("util");
-const { Telegraf } = require("telegraf");
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
+import dotenv from "dotenv";
+import { Telegraf } from "telegraf";
 
-require("dotenv").config();
+dotenv.config();
 
-const readFile = util.promisify(fs.readFile);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const bot = new Telegraf(process.env.SECRET_KEY);
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply("Welcome to Algo Bot by Shubham"));
-
+bot.start((ctx) => ctx.reply(`
+  Welcome to ALGO BOT.
+  You can ask leetcode question here.
+  Just give the question name Eg. 3sum
+  `))
 bot.command("whomadethis", (ctx) => ctx.reply("Shubham"));
-
 bot.command("hi", (ctx) => ctx.reply("You can ask leetcode questions here."));
 
 async function handleFile(ctx, fileName) {
   try {
     const filePath = path.join(__dirname, "solutions", `${fileName}.txt`);
-    const fileContent = await util.promisify(fs.readFile)(filePath, "utf8");
+    const fileContent = await fs.readFile(filePath, "utf8");
     ctx.reply(fileContent);
   } catch (error) {
     console.error("Error reading file:", error);
-    ctx.reply(
-      "Sorry, I couldn't read the file. Please make sure the file exists and try again."
-    );
+    ctx.reply("Sorry, I couldn't read the file. Please make sure the file exists and try again.");
   }
 }
 
 bot.on("text", async (ctx) => {
-  try {
-    const fileName = ctx.message.text.trim();
-    if (!fileName) {
-      ctx.reply("Please provide a valid file name.");
-      return;
-    }
-    await handleFile(ctx, fileName);
-  } catch (error) {
-    console.error("Error:", error);
-    ctx.reply("An error occurred. Please try again later.");
+  const fileName = ctx.message.text.trim();
+  if (!fileName) {
+    ctx.reply("Please provide a valid file name.");
+    return;
   }
+  await handleFile(ctx, fileName);
 });
 
 bot.on("sticker", (ctx) => ctx.reply("❤️"));
